54b29b8b2e9f28f8ec256191f35eeeb5f058cc05
Jeff Marks <jeff.t.marks@gmail.com>
2022-11-22 23:03:22 -0500

    completed basic auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,44 @@
 const User = require('../models/user');
+const { StatusCodes } = require('http-status-codes');
 
 const register = async (req, res) => {
-	res.status(200).json({msg: 'Register'});
+	try {
+		const user = await User.create(req.body);
+		const token = user.createJWT();
+		res.status(StatusCodes.OK).json({user: user, token});
+	} catch (error) {
+		res.status(StatusCodes.UNAUTHORIZED).json(error);
+	}
 };
 
 const login = async (req, res) => {
-	res.status(200).json({msg: 'Login'});
+	const { username, password } = req.body;
+	if (!username || !password) {
+		return res.status(StatusCodes.BAD_REQUEST).json({ msg: 'Please provide username and password'});
+	}
+
+	const user = await User.findOne({username})
+	if (!user) {
+		return res.status(StatusCodes.NOT_FOUND).json({ msg: 'Invalid credentials'});
+	}
+
+	const passwordIsMatch = await user.comparePassword(password);
+	if (!passwordIsMatch) {
+		return res.status(StatusCodes.NOT_FOUND).json({ msg: 'Invalid credentials'});
+	}
+
+	const token = user.createJWT();
+	res.status(StatusCodes.OK).json({user: user, token});
+
 };
 
 const profile = async (req, res) => {
-	res.status(200).json({msg: 'Get Profile'});
+	try {
+		const user = await User.findById(req.user.userId);
+		res.status(StatusCodes.OK).json(user);
+	} catch (error) {
+		res.status(StatusCodes.UNAUTHORIZED).json(error);
+	}
 };
 
 module.exports = {
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { register, login, profile } = require('../controllers/auth');
 
+const authenticateUser = require('../middleware/auth');
+
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login/', login);
-router.get('/profile', profile);
+router.get('/profile', authenticateUser, profile);
 
 module.exports = router;
 
